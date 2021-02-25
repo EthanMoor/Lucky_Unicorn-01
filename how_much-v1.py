dcb5ee42869bd92eb3baaa470d965ce0d09f3ae2
EthanMoor <79174862+EthanMoor@users.noreply.github.com>
2021-02-25 14:26:33 +1300

    money checker v1

---
+++ how_much-v1.py
@@ -0,0 +1,25 @@
+# Functions go here
+
+
+# Main Routine go here
+
+error = "Please enter an whole number between 1 and 10"
+
+valid = False
+while not valid:
+    try:
+        # Ask the question
+        response = int(input("How much would you "
+                             "like to play with? \n"
+                             "$"))
+        # If the amount is too low / too high give
+        if 0 < response <= 10:
+            print("You have asked to play "
+                  "with ${}".format(response))
+
+        # Output an error
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
