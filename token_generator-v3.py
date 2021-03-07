193bc83cafaf086afb3b2e4fc95c617c3994e881
EthanMoor <79174862+EthanMoor@users.noreply.github.com>
2021-03-08 09:09:07 +1300

    Rounds-v1 / token gen-v3

--- token_generator-v3.py
+++ token_generator-v3.py
@@ -10,13 +10,16 @@ for item in range(0,10):
     chosen_num = random.randint(1, 100)
 
     # Adjust Balance
+    # If the random # is between 1 and 6 User gets a Unicorn (add $4 to balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
         balance += 4
+    # If the random # is between 6 and 36 #  User gets a Donkey (Subtract $1 from balance)
     elif 6 <= chosen_num <= 36:
         chosen = "donkey"
         balance -= 1
     else:
+        # If the number is either a horse or zebra, in both cases, subtract $0.50 from the balance
         if chosen_num % 2 == 0:
             chosen = "horse"
         else:
